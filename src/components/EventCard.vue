5027ab63737568631e7f2f56c7ef37e6218865f0
Chris Bistline <chris.bistline@gmail.com>
2019-07-16 10:42:48 -0500

    setup

---
+++ src/components/EventCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 v-if="" @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+import { libName } from 'libName'
+
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      key: 'value'
+    }
+  },
+}
+</script>
+
+<style></style>
